df9820ceea65329e5c1063cfc82ff33d23f28e8f
DanteOnline <iamdanteonline@gmail.com>
2016-09-06 18:12:57 +0300

    group objects

--- init.py
+++ init.py
@@ -1,73 +1,149 @@
-#import bpy
-from core import delete_all
+import bpy
+from core import delete_all, select_by_names, deselect_by_names
 from materials import makeMaterial, setMaterial
 from meshes import create_simple_cube
 
 #Создание 3-х кубов подряд
 #count - количество кубов
+#возвращаем список имен объектов
 def create_cube_line(count, x=0,y=0,z=0,material=None):
     #шаг
     step = 2
     #делаем 4 куба
+    name_list = []
     for i in range(count):
-        create_simple_cube((x+step*i,y,z),material)
+        cube = create_simple_cube((x+step*i,y,z))
+        name_list.append(cube.name)
+        setMaterial(cube, material)
+    return name_list
 
 #Создаем палку из 4-х кубиков
 def create_line(x=0,y=0,z=0):
     #палка будет красной
-    red = makeMaterial('Red', (1,0,0), (1,1,1), 1)
-    create_cube_line(4,x,y,z,red)
+    material = makeMaterial('Red', (1,0,0), (1,1,1), 1)
+    name_list = create_cube_line(4,x,y,z,material)
+    #снимаем отметку со всех элементов
+    bpy.ops.object.select_all(action='TOGGLE')
+    #выделяем нужные кубы по имени
+    select_by_names(name_list)
+    bpy.ops.object.join()
         
 #создаем букву г
 def create_g_r(x=0,y=0,z=0):
     #сначала создаем палку из 3-х кубов
     #желтая
     material = makeMaterial('Yellow', (1,1,0), (1,1,1), 1)
-    create_cube_line(3,x,y,z,material)
+    name_list = create_cube_line(3,x,y,z,material)
     #добавляем куб слева
-    create_simple_cube((x,y+2,z),material)
+    cube = create_simple_cube((x,y+2,z))
+    name_list.append(cube.name)
+    setMaterial(cube,material)
+    
+    #снимаем отметку со всех элементов
+    bpy.ops.object.select_all(action='TOGGLE')
+    #выделяем нужные кубы по имени
+    select_by_names(name_list)
+    bpy.ops.object.join()
     
 def create_g_l(x=0,y=0,z=0):
     #фиолетовая
     material = makeMaterial('Violet', (1,0,1), (1,1,1), 1)
     #сначала создаем палку из 3-х кубов
-    create_cube_line(3,x,y,z,material)
+    name_list = create_cube_line(3,x,y,z,material)
     #добавляем куб слева
-    create_simple_cube((x,y-2,z),material)
+    cube = create_simple_cube((x,y-2,z))
+    name_list.append(cube.name)
+    setMaterial(cube,material)
+    
+    #снимаем отметку со всех элементов
+    bpy.ops.object.select_all(action='TOGGLE')
+    #выделяем нужные кубы по имени
+    select_by_names(name_list)
+    bpy.ops.object.join()
     
 #создаем штуку с кубиком по середине (крест)
 def create_cross(x=0,y=0,z=0):
     material = makeMaterial('Green', (0,1,0), (1,1,1), 1)
     #сначала создаем палку из 3-х кубов
-    create_cube_line(3,x,y,z,material)
+    name_list = create_cube_line(3,x,y,z,material)
     #добавляем куб по середине
-    create_simple_cube((x+2,y-2,z),material)
+    cube = create_simple_cube((x+2,y-2,z))
+    name_list.append(cube.name)
+    setMaterial(cube,material)
+    
+    #снимаем отметку со всех элементов
+    bpy.ops.object.select_all(action='TOGGLE')
+    #выделяем нужные кубы по имени
+    select_by_names(name_list)
+    bpy.ops.object.join()
     
 #создаем кубик
 def create_cube4(x=0,y=0,z=0):
+    name_list = []
     #фиолетовая
     material = makeMaterial('Blue', (0,0,1), (1,1,1), 1)
-    create_simple_cube((x,y,z),material)
-    create_simple_cube((x+2,y,z),material)
-    create_simple_cube((x,y+2,z),material)
-    create_simple_cube((x+2,y+2,z),material)
+    cube = create_simple_cube((x,y,z))
+    name_list.append(cube.name)
+    setMaterial(cube,material)
+    cube = create_simple_cube((x+2,y,z))
+    name_list.append(cube.name)
+    setMaterial(cube,material)
+    cube = create_simple_cube((x,y+2,z))
+    name_list.append(cube.name)
+    setMaterial(cube,material)
+    cube = create_simple_cube((x+2,y+2,z))
+    name_list.append(cube.name)
+    setMaterial(cube,material)
+    #снимаем отметку со всех элементов
+    bpy.ops.object.select_all(action='TOGGLE')
+    #выделяем нужные кубы по имени
+    select_by_names(name_list)
+    bpy.ops.object.join()
     
 #создаем буквы z
 def create_z1(x=0,y=0,z=0):
+    name_list = []
     #светло голубая
     material = makeMaterial('ligth blue', (0,1,1), (1,1,1), 1)
-    create_simple_cube((x,y,z),material)
-    create_simple_cube((x+2,y,z),material)
-    create_simple_cube((x+2,y+2,z),material)
-    create_simple_cube((x+4,y+2,z),material)
+    cube = create_simple_cube((x,y,z))
+    name_list.append(cube.name)
+    setMaterial(cube,material)
+    cube = create_simple_cube((x+2,y,z))
+    name_list.append(cube.name)
+    setMaterial(cube,material)
+    cube = create_simple_cube((x+2,y+2,z))
+    name_list.append(cube.name)
+    setMaterial(cube,material)
+    cube = create_simple_cube((x+4,y+2,z))
+    name_list.append(cube.name)
+    setMaterial(cube,material)
+     #снимаем отметку со всех элементов
+    bpy.ops.object.select_all(action='TOGGLE')
+    #выделяем нужные кубы по имени
+    select_by_names(name_list)
+    bpy.ops.object.join()
     
 def create_z2(x=0,y=0,z=0):
     #светло голубая
+    name_list = []
     material = makeMaterial('ligth blue2', (0,0.5,1), (1,1,1), 1)
-    create_simple_cube((x+2,y,z),material)
-    create_simple_cube((x+4,y,z),material)
-    create_simple_cube((x+2,y+2,z),material)
-    create_simple_cube((x,y+2,z),material)
+    cube = create_simple_cube((x+2,y,z))
+    name_list.append(cube.name)
+    setMaterial(cube,material)
+    cube = create_simple_cube((x+4,y,z))
+    name_list.append(cube.name)
+    setMaterial(cube,material)
+    cube = create_simple_cube((x+2,y+2,z))
+    name_list.append(cube.name)
+    setMaterial(cube,material)
+    cube = create_simple_cube((x,y+2,z))
+    name_list.append(cube.name)
+    setMaterial(cube,material)
+     #снимаем отметку со всех элементов
+    bpy.ops.object.select_all(action='TOGGLE')
+    #выделяем нужные кубы по имени
+    select_by_names(name_list)
+    bpy.ops.object.join()
         
 if __name__ == '__main__':
     #удаляем все объекты
--- meshes.py
+++ meshes.py
@@ -1,14 +1,17 @@
 import bpy
 from core import delete_all
-from materials import makeMaterial, setMaterial
 
-def create_simple_cube(location, material=None):
+def create_simple_cube(location):
     bpy.ops.mesh.primitive_cube_add(location=location)
-    if material != None:
-        setMaterial(bpy.context.object, material)
+    return bpy.context.object
+
+def create_simple_plane(location):
+    bpy.ops.mesh.primitive_plane_add(location=location)
+    plane = bpy.context.object
+   
+    return plane
     
 if __name__ == '__main__':
     delete_all()
     create_simple_cube((0,0,0))
-    red = makeMaterial('Red', (1,0,0), (1,1,1), 1)
-    create_simple_cube((3,3,0),red)
+    create_simple_plane((1,1,1))
